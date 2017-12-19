4e6e88d1588b0d39e495b8f6a5c2854fc2a1198a
andreas <vlachos.andreas@gmail.com>
2017-12-19 04:34:44 +0200

    Several updates

---
+++ postgres/init.d/ats.sql
@@ -0,0 +1,3 @@
+CREATE USER atsowner WITH PASSWORD 'atsowner';
+CREATE DATABASE ats_intralot;
+GRANT ALL PRIVILEGES ON DATABASE ats_intralot TO atsowner;
---
+++ postgres/init.d/betcatcher.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE ats_betcatch_intralot;
+GRANT ALL PRIVILEGES ON DATABASE ats_betcatch_intralot TO atsowner;
